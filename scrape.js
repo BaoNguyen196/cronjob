bea6ef90b7adaa4ff6cc85e2b1fdc476b16fa756
Bao Nguyen <bao.nguyen2@trustingsocial.com>
2024-09-16 14:57:37 +0700

    update path

--- scrape.js
+++ scrape.js
@@ -50,7 +50,7 @@ const fs = require('fs').promises;
   // Save to Strapi or handle data as needed here
   await writeDataToFile({
     data: offers,
-    path: 'src/data/vi',
+    path: '/',
     fileName: 'offers',
   });
   await browser.close();
